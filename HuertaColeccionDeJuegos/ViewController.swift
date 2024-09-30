4add12376041158651d439ded95fdd6f878c01e0
L-E-Huerta-tec <luis.huerta@tecsup.edu.pe>
2024-09-30 17:03:01 -0500

    Initial Commit

---
+++ HuertaColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HuertaColeccionDeJuegos
+//
+//  Created by Luia Enrique Huerta Gonzales on 30/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
